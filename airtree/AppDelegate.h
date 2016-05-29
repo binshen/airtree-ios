610aad68eafa4dd81872a90f86ccf38a96470246
bin.shen <bin.shen@openx.com>
2016-05-29 09:14:35 +0800

    Initial Commit

---
+++ airtree/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  airtree
+//
+//  Created by Bin Shen on 5/29/16.
+//  Copyright © 2016 Bin Shen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ airtree/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  airtree
+//
+//  Created by Bin Shen on 5/29/16.
+//  Copyright © 2016 Bin Shen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
