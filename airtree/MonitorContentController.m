9f9506cb202911c979d48be2b82c38b3fbf975b5
bin.shen <bin.shen@openx.com>
2016-07-14 02:21:01 +0800

    111

--- airtree/MonitorContentController.m
+++ airtree/MonitorContentController.m
@@ -28,12 +28,6 @@
 - (void) initViews:(NSUInteger) pageIndex withDevice:(NSDictionary *) device {
     //NSLog(@"%@", device);
     
-//    CGRect rect = self.ImgChart.bounds;
-//    self.ImgChart.frame = CGRectMake(CGRectGetMidX(self.view.bounds) - CGRectGetMidX(rect), CGRectGetMidX(self.view.bounds) - CGRectGetMidX(rect) + self.ImgChart.frame.size.width, self.ImgChart.frame.size.width, self.ImgChart.frame.size.height);
-//    self.ImgChart.autoresizingMask = UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleRightMargin;
-    
-    [self.ImgChart.image drawInRect:CGRectMake((self.view.frame.size.width/2) - (self.ImgChart.image.size.width/2), (self.view.frame.size.height / 2) - (self.ImgChart.image.size.height / 2), self.ImgChart.image.size.width, self.ImgChart.image.size.height)];
-    
     if((NSNull *) device[@"data"] == [NSNull null] || device[@"data"] == nil) {
         self.LabelCreatedTime.text = @"0000-00-00 00:00:00";
         self.LabelTop.text = @"";
@@ -50,22 +44,22 @@
             self.LabelTop.text = @"PM2.5";
             self.LabelMain.text = [NSString stringWithFormat:@"%@", data[@"x1"]];
             self.LabelBottom.text = @"ug/m³";
-            [self.ImgChart setImage:[UIImage imageNamed:@"bg_pm.png"]];
+            [self.ImgChart setImage:[UIImage imageNamed:@"bg_pm_s.png"]];
         } else if(pageIndex == 1) {
             self.LabelTop.text = @"温度";
             self.LabelMain.text = [NSString stringWithFormat:@"%@", data[@"x11"]];
             self.LabelBottom.text = @"℃";
-            [self.ImgChart setImage:[UIImage imageNamed:@"bg_wendu.png"]];
+            [self.ImgChart setImage:[UIImage imageNamed:@"bg_wendu_s.png"]];
         } else if(pageIndex == 2) {
             self.LabelTop.text = @"湿度";
             self.LabelMain.text = [NSString stringWithFormat:@"%@", data[@"x10"]];
             self.LabelBottom.text = @"%";
-            [self.ImgChart setImage:[UIImage imageNamed:@"bg_shidu.png"]];
+            [self.ImgChart setImage:[UIImage imageNamed:@"bg_shidu_s.png"]];
         } else {
             self.LabelTop.text = @"甲醛";
             self.LabelMain.text = [NSString stringWithFormat:@"%@", data[@"x9"]];
             self.LabelBottom.text = @"mg/m³";
-            [self.ImgChart setImage:[UIImage imageNamed:@"bg_jiaquan.png"]];
+            [self.ImgChart setImage:[UIImage imageNamed:@"bg_jiaquan_s.png"]];
         }
     }
 }
